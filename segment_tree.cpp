91b55014a70b5d378c0acc2a6b38bf99f45e4d79
Tomasz Palys <tomek201992@gmail.com>
2021-01-16 17:12:01 +0100

    Implement sum segment tree

---
+++ segment_tree.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+
+using namespace std;
+
+class segment_tree {
+    int * t;
+    int n;
+public:
+    segment_tree(int n);
+    ~segment_tree();
+    void modify(int k, int x);
+    int query(int a, int b);
+};
+
+segment_tree::segment_tree(int n): n(n) {
+    t = new int[2 * n];
+    for (int i = 0; i < 2 * n; i++) {
+        t[i] = 0;
+    }
+}
+
+segment_tree::~segment_tree() {
+    delete[] t;
+}
+
+void segment_tree::modify(int k, int x) {
+    k += n;
+    t[k] += x;
+    for (k /= 2; k >= 1; k /= 2) {
+        t[k] = t[2 * k] + t[2 * k + 1];
+    }
+}
+
+int segment_tree::query(int a, int b) {
+    a += n;
+    b += n;
+    int s = 0;
+    while (a <= b) {
+        if (a % 2 == 1) {
+            s += t[a++];
+        }
+        if (b % 2 == 0) {
+            s += t[b--];
+        }
+        a /= 2;
+        b /= 2;
+    }
+    return s;
+}
+
+int main() {
+    segment_tree t(32);
+    cout << t.query(5, 15) << "\n";
+    t.modify(1, 10);
+    t.modify(5, 7);
+    cout << t.query(1, 20) << " " << t.query(2, 5) << " " << t.query(1, 4) << " " << t.query(8, 15) << "\n";
+    t.modify(8, 10);
+    t.modify(1, 2);
+    t.modify(20, 1);
+    cout << t.query(1, 20) << " " << t.query(2, 5) << " " << t.query(1, 4) << " " << t.query(8, 15) << "\n";
+    return 0;
+}

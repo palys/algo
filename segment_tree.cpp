554781f4c3d5f646b3bab5c60c4e4e99956ecdd8
Tomasz Palys <tomek201992@gmail.com>
2021-01-16 17:26:09 +0100

    Accept operation as constructor parameter

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -5,14 +5,15 @@ using namespace std;
 class segment_tree {
     int * t;
     int n;
+    int (*f) (int, int);
 public:
-    segment_tree(int n);
+    segment_tree(int n, int (*f) (int, int));
     ~segment_tree();
-    void modify(int k, int x);
+    void add(int k, int x);
     int query(int a, int b);
 };
 
-segment_tree::segment_tree(int n): n(n) {
+segment_tree::segment_tree(int n, int (*f) (int, int)): n(n), f(f) {
     t = new int[2 * n];
     for (int i = 0; i < 2 * n; i++) {
         t[i] = 0;
@@ -23,11 +24,11 @@ segment_tree::~segment_tree() {
     delete[] t;
 }
 
-void segment_tree::modify(int k, int x) {
+void segment_tree::add(int k, int x) {
     k += n;
     t[k] += x;
     for (k >>= 1; k >= 1; k >>= 1) {
-        t[k] = t[k << 1] + t[(k << 1) + 1];
+        t[k] = f(t[k << 1], t[(k << 1) + 1]);
     }
 }
 
@@ -37,10 +38,10 @@ int segment_tree::query(int a, int b) {
     int s = 0;
     while (a <= b) {
         if (a & 1) {
-            s += t[a++];
+            s = f(s, t[a++]);
         }
         if (!(b & 1)) {
-            s += t[b--];
+            s = f(s, t[b--]);
         }
         a >>= 1;
         b >>= 1;
@@ -48,15 +49,28 @@ int segment_tree::query(int a, int b) {
     return s;
 }
 
-int main() {
-    segment_tree t(32);
+int add(int a, int b) {
+    return a + b;
+}
+
+int max(int a, int b) {
+    return a < b ? b : a;
+}
+
+void test(segment_tree & t) {
     cout << t.query(5, 15) << "\n";
-    t.modify(1, 10);
-    t.modify(5, 7);
+    t.add(1, 10);
+    t.add(5, 7);
     cout << t.query(1, 20) << " " << t.query(2, 5) << " " << t.query(1, 4) << " " << t.query(8, 15) << "\n";
-    t.modify(8, 10);
-    t.modify(1, 2);
-    t.modify(20, 1);
+    t.add(8, 10);
+    t.add(1, 2);
+    t.add(20, 1);
     cout << t.query(1, 20) << " " << t.query(2, 5) << " " << t.query(1, 4) << " " << t.query(8, 15) << "\n";
+}
+
+int main() {
+    segment_tree t_sum(32, add), t_max(64, max);
+    test(t_sum);
+    test(t_max);
     return 0;
 }
